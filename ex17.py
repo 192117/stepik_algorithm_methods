9f70d0972af1da5caf9cf843cab84b1a0a3211cd
Kokoc <noti1996@gmail.com>
2022-05-12 00:14:17 +0300

    Доделать 17 задание.

--- ex17.py
+++ ex17.py
@@ -10,14 +10,25 @@ def gis(A):
         right = len(A)
         while right - left > 1:
             middle = (left + right) // 2
-            if F[middle] < A[i]:
-                right = middle
-            else:
+            if F[middle] >= A[i]:
                 left = middle
+            else:
+                right = middle
         F[right] = A[i]
         pos[right] = i
     print(F)
     print(pos)
+    k = -1
+    for i in range(len(F)-1,-1, -1):
+        if F[i] != float('-inf'):
+            k = i
+            break
+    print(k)
+    answer = [i+1,]
+    for i in range(k-1, -1, -1):
+        if A[i] >= A[i+1]:
+            answer.append(i+1)
+    print(*answer[::-1], end=' ')
 
 
 gis(A)
