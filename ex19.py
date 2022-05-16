65173f68a43be20dcba5e069db90fc92ba6c4683
Kokoc <noti1996@gmail.com>
2022-05-16 06:43:43 +0300

    По 19 задачу.

--- ex19.py
+++ ex19.py
@@ -1,5 +1,18 @@
 W, n = map(int, input().split())
 p = list(map(int, input().split()))
+p.sort()
 
-def knapsackwithoutrepsbu():
-    pass()
+def knapsackwithoutrepsbu(W, n, p):
+    D = [[0 for i in range(W+1)] for j in range(n+1)]
+    for i in range(1, n+1):
+        for w in range(1, W+1):
+            if p[i-1] > w:
+                D[i][w] = D[i-1][w]
+            else:
+                D[i][w] = max(D[i-1][w], D[i-1][w-p[i-1]] + p[i-1])
+    print(D)
+    return D[n][W]
+
+
+print(knapsackwithoutrepsbu(W, n, p))
+
