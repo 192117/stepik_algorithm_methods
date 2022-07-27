98178d46bf39c0633ff91d71bec442820bc3b779
Kokoc <noti1996@gmail.com>
2022-07-27 23:52:09 +0300

    24 в процессе.

--- ex24.py
+++ ex24.py
@@ -2,11 +2,21 @@ size, n = map(int, input().split())
 
 stack = []
 
+def buffer(arrival, duration, size, stack):
+    if len(stack) == 0:
+        stack.append((arrival, arrival+duration))
+    elif len(stack) < size:
+        stack.append((arrival, arrival+duration))
+    else:
+        current = stack.pop(0)
+        while current[1] < arrival:
+             print(current[0])
+             stack.append((arrival, arrival+duration))
+             current = stack.pop(0)
+        else:
+            print(-1)
+
+
 for i in range(n):
     arrival, duration = map(int, input().split())
-    if len(stack) == 0:
-        stack.append((arrival, duration))
-        print(arrival)
-    elif len(stack) <= size:
-        current = stack.pop
-
+    buffer(arrival, duration, size, stack)
