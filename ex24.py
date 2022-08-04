ad60f01f5bac163ff3455763ff25957ef7dce6f1
Kokoc <noti1996@gmail.com>
2022-08-05 00:19:05 +0300

    Пофиксить в 24 задаче подсчет времени начала выполнения пакета.

--- ex24.py
+++ ex24.py
@@ -2,21 +2,49 @@ size, n = map(int, input().split())
 
 stack = []
 
-def buffer(arrival, duration, size, stack):
-    if len(stack) == 0:
-        stack.append((arrival, arrival+duration))
-    elif len(stack) < size:
-        stack.append((arrival, arrival+duration))
-    else:
-        current = stack.pop(0)
-        while current[1] < arrival:
-             print(current[0])
-             stack.append((arrival, arrival+duration))
-             current = stack.pop(0)
+def buffer(packages, size, stack):
+    time = 0
+    process_time = 0
+    for arrival, duration in packages:
+        if len(stack) == 0:
+            stack.append((arrival, duration, arrival))
+            print(stack[-1][2])
+            time = arrival
+            process_time = duration
+        elif len(stack) < size:
+            if arrival >= time + process_time:
+                stack.append((arrival, duration, arrival))
+                print(stack[-1][2])
+                time = arrival
+                process_time = duration
+            else:
+                stack.append((arrival, duration, arrival+process_time))
+                time = arrival+process_time
+                process_time = duration
+                print(stack[-1][2])
         else:
-            print(-1)
+            if arrival >= time + process_time:
+                procces = stack[0]
+                while procces[0] + procces[1] <= arrival:
+                    stack.pop(0)
+                    if len(stack) != 0:
+                        procces = stack[0]
+                    else:
+                        break
+                stack.append((arrival, duration, arrival))
+                print(stack[-1][2])
+                time = arrival
+                process_time = duration
+            else:
+                print(-1)
 
+
+packages = []
 
 for i in range(n):
     arrival, duration = map(int, input().split())
-    buffer(arrival, duration, size, stack)
+    packages.append((arrival, duration))
+
+
+buffer(packages, size, stack)
+
