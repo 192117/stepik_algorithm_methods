d81a1210fe31b68c0bd6f96049399ec536133271
Kokoc <noti1996@gmail.com>
2022-08-08 22:33:23 +0300

    24 готова. 25 старт.

--- ex24.py
+++ ex24.py
@@ -1,50 +1,80 @@
-size, n = map(int, input().split())
+# size, n = map(int, input().split())
+#
+# stack = []
+#
+# def buffer(packages, size, stack):
+#     start_time = 0
+#     end_time = 0
+#     for arrival, duration in packages:
+#         if len(stack) == 0:
+#             start_time = arrival
+#             end_time = start_time + duration
+#             stack.append((start_time, end_time))
+#             print(start_time)
+#         elif len(stack) < size:
+#             if end_time <= arrival:
+#                 start_time = arrival
+#                 end_time = start_time + duration
+#                 stack.append((start_time, end_time))
+#                 print(start_time)
+#             else:
+#                 start_time = end_time
+#                 end_time = start_time + duration
+#                 stack.append((start_time, end_time))
+#                 print(start_time)
+#         else:
+#             current = stack[0]
+#             while current[1] <= arrival:
+#                 stack.pop(0)
+#                 if len(stack) != 0:
+#                     current = stack[0]
+#                     start_time, end_time = stack[-1][0], stack[-1][1]
+#                 else:
+#                     break
+#             if len(stack) < size:
+#                 if end_time <= arrival:
+#                     start_time = arrival
+#                     end_time = start_time + duration
+#                     stack.append((start_time, end_time))
+#                     print(start_time)
+#                 else:
+#                     start_time = end_time
+#                     end_time = start_time + duration
+#                     stack.append((start_time, end_time))
+#                     print(start_time)
+#             else:
+#                 print(-1)
+#
+#
+#
+# packages = []
+#
+# for i in range(n):
+#     arrival, duration = map(int, input().split())
+#     packages.append((arrival, duration))
+#
+#
+# buffer(packages, size, stack)
 
-stack = []
-
-def buffer(packages, size, stack):
-    time = 0
-    process_time = 0
-    for arrival, duration in packages:
-        if len(stack) == 0:
-            stack.append((arrival, duration, arrival))
-            print(stack[-1][2])
-            time = arrival
-            process_time = duration
-        elif len(stack) < size:
-            if arrival >= time + process_time:
-                stack.append((arrival, duration, arrival))
-                print(stack[-1][2])
-                time = arrival
-                process_time = duration
-            else:
-                stack.append((arrival, duration, arrival+process_time))
-                time = arrival+process_time
-                process_time = duration
-                print(stack[-1][2])
-        else:
-            if arrival >= time + process_time:
-                procces = stack[0]
-                while procces[0] + procces[1] <= arrival:
-                    stack.pop(0)
-                    if len(stack) != 0:
-                        procces = stack[0]
-                    else:
-                        break
-                stack.append((arrival, duration, arrival))
-                print(stack[-1][2])
-                time = arrival
-                process_time = duration
-            else:
-                print(-1)
-
-
-packages = []
-
-for i in range(n):
-    arrival, duration = map(int, input().split())
-    packages.append((arrival, duration))
-
-
-buffer(packages, size, stack)
-
+# Самый элегантный код из решения.
+# from collections import deque
+#
+# packages = []
+#
+# size, n = map(int, input().split())
+#
+# for i in range(n):
+#     arrival, duration = map(int, input().split())
+#     packages.append((arrival, duration))
+#
+# times = deque()
+# for a, d in packages:
+#     while times and times[0] <= a:
+#             times.popleft()
+#     if len(times) < size:
+#         if times:
+#             a = max(a, times[-1])
+#         print(a)
+#         times.append(a+d)
+#     else:
+#         print(-1)
