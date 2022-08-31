a572a2a6e344f02c0ffc0f4f79e7726648e6295c
Kokoc <noti1996@gmail.com>
2022-08-31 22:23:55 +0300

    34 набросок.

---
+++ ex34.py
@@ -0,0 +1,12 @@
+# https://pythonist.ru/obhod-dvoichnogo-dereva-na-python/
+n = int(input())
+
+class TreeNode:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+
+
